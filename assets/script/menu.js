fae48d0015dbfa732729cf0ef55d24924273dae4
CillianBgrPro <baugercillianpro@gmail.com>
2026-05-05 14:04:10 +0200

    fix header

--- assets/script/menu.js
+++ assets/script/menu.js
@@ -3,6 +3,22 @@ const closeButton = document.getElementById("closeButton");
 const burgerMenu = document.getElementById("burgerMenu");
 const menuOverlay = document.getElementById("menuOverlay");
 
+const currentPage = window.location.pathname.split("/").pop();
+const menuLinks = document.querySelectorAll(".menu-links a");
+
+menuLinks.forEach((link) => {
+  link.classList.remove("active");
+
+  const linkPage = link.getAttribute("href");
+
+  if (
+    linkPage === currentPage ||
+    (currentPage === "formation_pratique.html" && linkPage === "formations.html")
+  ) {
+    link.classList.add("active");
+  }
+});
+
 burgerButton.addEventListener("click", () => {
   burgerMenu.classList.add("open");
   menuOverlay.classList.add("open");
